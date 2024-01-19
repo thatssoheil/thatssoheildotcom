7184ab0da34e8d1200c2404e59e45fd65e735291
Thatssoheil <soheil.fakour@gmail.com>
2024-01-19 19:14:15 +0330

    fix/homepage subtitle size changed

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,11 @@
-import { Heading } from '@/components/core/Typography'
+import { Heading, Paragraph } from '@/components/core/Typography'
 
 export default function Home() {
   return (
     <main>
       <div className='flex flex-col items-center justify-center h-screen py-24'>
         <Heading className='text-center'>{`I'm`} busy building something awesome!</Heading>
-        <Heading>Come back later.</Heading>
+        <Paragraph>Come back later.</Paragraph>
       </div>
     </main>
   )
